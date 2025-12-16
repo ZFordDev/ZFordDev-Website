d26d7ef2e9e9de54c20bdc1ab3d8eb86de5cae7d
Zachary Ford <zachary.ford8@gmail.com>
2025-12-16 18:04:54 +1000

    role back to work with new domain

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
-    pathPrefix: "/ZFordDev-Website/",
     dir: {
       input: "src",
       includes: "_includes",
